bf9a4529bff7cf566dba3b405c851aac17d19aca
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-10-22 07:25:46 -0500

    Task 0 message updated

--- Task0.py
+++ Task0.py
@@ -19,5 +19,6 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-print(texts[0][0], 'texts', texts[0][1], 'at time', texts[0][2])
-print(calls[-1][0], 'calls', calls[-1][1], 'at time', calls[-1][2], 'lasting', calls[-1][3], 'seconds')
+print('First record of texts,', texts[0][0], 'texts', texts[0][1], 'at time', texts[0][2])
+print('Last record of calls,', calls[-1][0], 'calls', calls[-1][1], 'at time', calls[-1][2], 'lasting', calls[-1][3],
+      'seconds')
